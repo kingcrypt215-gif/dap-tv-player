9ce20f94601d979b78213184e84eabfe49717219
kingcrypt215-gif <kingcrypt215@gmail.com>
2026-07-19 15:46:27 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,1 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('dap-store').then((cache) => cache.addAll(['./'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
-});
 self.addEventListener('fetch', function(event) { event.respondWith(fetch(event.request)); });
